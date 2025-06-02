ccc57a4a6eeb8a46295e57b2f1f964fd0606b7b2
Your Name <you@example.com>
2025-06-02 23:10:10 +0300

    feat: добавить все размеры favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,10 +10,13 @@ export default defineNuxtConfig({
     pageTransition: { name: "page", mode: "out-in" },
     head: {
       link: [
-        { rel: "icon", type: "image/png", href: "/favicon-96x96.png", sizes: "96x96" },
         { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
         { rel: "shortcut icon", href: "/favicon.ico" },
         { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+        { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
+        { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
+        { rel: "icon", type: "image/png", sizes: "64x64", href: "/favicon-64x64.png" },
+        { rel: "icon", type: "image/png", sizes: "96x96", href: "/favicon-96x96.png" },
         { rel: "manifest", href: "/site.webmanifest" },
       ],
       meta: [
